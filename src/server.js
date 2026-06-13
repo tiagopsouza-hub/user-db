9927228ec82754707a7b982bdb5d3d57e210d39e
= <=>
2026-06-13 11:54:27 -0300

    Implementação do tratamento de erros

--- src/server.js
+++ src/server.js
@@ -18,46 +18,78 @@ const pool = mysql.createPool({
 
 // C
 app.post('/users', async (req, res) => {
-    const nome = req.body.nome;
-    const email = req.body.email;
-    const cpf = req.body.cpf;
-    const apelido = req.body.apelido ?? null;
+    try{
+        const nome = req.body.nome;
+        const email = req.body.email;
+        const cpf = req.body.cpf;
+        const apelido = req.body.apelido ?? null;
 
-    const result = await pool.query(
+        const result = await pool.query(
         'INSERT INTO user (nome, email, cpf, apelido) VALUES(?,?,?,?);',
         [nome, email, cpf, apelido]
     );
 
-    res.status(201).json({msg: "Usuário criado com sucesso"});
+        res.status(201).json({msg: "Usuário criado com sucesso"});
+    }
+    catch (erro) {
+        console.error(erro)
+        res.status(500).json({msg: "Erro ao criar usuário"});
+    }
 })
 
 // R
 app.get('/users', async (req, res) => {
-    const rows = await pool.query('SELECT * FROM user;');
-    res.status(200).json(rows[0]);
+    try {
+        const rows = await pool.query('SELECT * FROM user;');
+        res.status(200).json(rows[0]);
+    }
+    catch (erro) {
+        console.error(erro);
+        res.status(500).json({msg: "Erro ao listar usuários"});
+    }
 })
 
 // U
 app.put('/users/:id', async (req, res) => {
-    const id = req.params.id;
-    const nome = req.body.nome;
-    const email = req.body.email;
-    const cpf = req.body.cpf;
-    const apelido = req.body.apelido ?? null;
+   try {
+        const id = req.params.id;
+        const nome = req.body.nome;
+        const email = req.body.email;
+        const cpf = req.body.cpf;
+        const apelido = req.body.apelido ?? null;
 
-    const update = await pool.query('UPDATE user SET nome = ?, email = ?, cpf = ?, apelido = ? WHERE id = ?;',
+        const user = await pool.query('SELECT * FROM user WHERE id = ?;',
+        [id]
+        );
+
+        if(user[0].length == 0){
+            throw new Error("Usuário não existe!");
+        }
+        
+        const update = await pool.query('UPDATE user SET nome = ?, email = ?, cpf = ?, apelido = ? WHERE id = ?;',
         [nome,email,cpf,apelido, id]
-    );
-    res.status(200).json({msg: "Usuário atualizado com sucesso!"})
+        );
+        res.status(200).json({msg: "Usuário atualizado com sucesso!"})
+    } 
+    catch (erro) {
+        console.error(erro)
+        res.status(500).json({msg: "Erro ao atualizar usuário"});
+    }
 })
 
 // D
 app.delete('/users/:id', async (req, res) => {
-    const id = req.params.id
-    const rows = await pool.query('DELETE FROM user WHERE id = ?;',
+    try{
+        const id = req.params.id
+        const rows = await pool.query('DELETE FROM user WHERE id = ?;',
         [id]
-    );
-    res.status(200).json({msg: "Usuário apagado com sucesso!"});
+        );
+        res.status(200).json({msg: "Usuário apagado com sucesso!"});
+    }
+    catch (erro) {
+        console.error(erro)
+        res.status(500).json({msg: "Erro ao deletar usuário"});
+    }
 })
 
 app.listen(port, () => {
